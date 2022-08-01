75a99c2bf957842e37afa27768ec3bbbe7a66008
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2022-08-01 14:36:47 +0300

    Create Chapter 3 Lesson 4-1.cpp

---
+++ Sprint 1/Chapter 3 Lesson 4-1.cpp
@@ -0,0 +1,11 @@
+struct Document {
+    int id;
+    int relevance;
+};
+
+bool HasDocumentGreaterRelevance(const Document& lhs, const Document& rhs) {
+    return lhs.relevance > rhs.relevance;
+}
+
+int main() {
+}
