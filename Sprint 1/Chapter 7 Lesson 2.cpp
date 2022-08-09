b5a7f41aa78f485bba8d19e54fa6ecede3add236
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2022-08-09 09:41:19 +0300

    Create Chapter 7 Lesson 2.cpp

---
+++ Sprint 1/Chapter 7 Lesson 2.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Document {
+    int id;
+    double relevance;
+    int rating;
+};
+
+void SortDocuments(vector<Document>& matched_documents) {            
+    sort(matched_documents.begin(), matched_documents.end(),
+         [](const Document& lhs, const Document& rhs) {
+             return lhs.rating > rhs.rating || (lhs.rating == rhs.rating && lhs.relevance > rhs.relevance);
+         });
+}
+
+int main() {
+    vector<Document> documents = {
+        {100, 0.5, 4},
+        {101, 1.2, 4},
+        {102, 0.3, 5}
+    };
+    SortDocuments(documents);
+    for (const Document& document : documents) {
+        cout << document.id << ' '
+             << document.relevance << ' '
+             << document.rating << endl;
+    }
+}
