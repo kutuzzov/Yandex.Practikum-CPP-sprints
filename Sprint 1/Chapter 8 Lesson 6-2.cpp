ce8f1f4b7a308be9988d375786ee26b34de52eb6
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2022-08-16 12:48:45 +0300

    Create Chapter 8 Lesson 6-2.cpp

---
+++ Sprint 1/Chapter 8 Lesson 6-2.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Animal {
+    string name;
+    int age;
+    double weight;
+};
+
+template <typename Container, typename KeyMapper>
+void SortBy(Container& container, KeyMapper key_mapper, bool reverse = false) {
+    // теперь можно сортировать контейнер
+    // с компаратором key_mapper(lhs) < key_mapper(rhs)
+    if (reverse) {
+        sort(container.begin(), container.end(),
+            [key_mapper](const auto& lhs, const auto& rhs) {
+                return key_mapper(lhs) > key_mapper(rhs);
+            });
+    }
+    else {
+        sort(container.begin(), container.end(),
+            [key_mapper](const auto& lhs, const auto& rhs) {
+                return key_mapper(lhs) < key_mapper(rhs);
+            });
+    }
+}
+
+void PrintNames(const vector<Animal>& animals) {
+    for (const Animal& animal : animals) {
+        cout << animal.name << ' ';
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<Animal> animals = {
+        {"Мурка"s,   10, 5},
+        {"Белка"s,   5,  1.5},
+        {"Георгий"s, 2,  4.5},
+        {"Рюрик"s,   12, 3.1},
+    };
+
+    PrintNames(animals);
+
+    SortBy(animals, [](const Animal& animal) { return animal.name; }, true);
+    PrintNames(animals);
+
+    SortBy(animals, [](const Animal& animal) { return animal.weight; });
+    PrintNames(animals);
+
+    return 0;
+}
