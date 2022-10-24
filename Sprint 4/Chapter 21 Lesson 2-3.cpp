83627f8db92a039cd9f3f9daf5f99a3818f421ad
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2022-10-24 14:32:23 +0300

    Create Chapter 21 Lesson 2-3.cpp

---
+++ Sprint 4/Chapter 21 Lesson 2-3.cpp
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename It>
+void PrintRange(It range_begin, It range_end) {
+    for (auto it = range_begin; it != range_end; ++it) {
+        cout << *it << " "s;
+    }
+    cout << endl;
+}
+
+template <typename Type>
+class Stack {
+public:
+    void Push(const Type& element) {
+        elements_.push_back(element);
+    }
+    void Pop() {
+        elements_.pop_back();
+    }
+    const Type& Peek() const {
+        return elements_.back();
+    }
+    Type& Peek() {
+        return elements_.back();
+    }
+    void Print() const {
+        PrintRange(elements_.begin(), elements_.end());
+    }
+    uint64_t Size() const {
+        return elements_.size();
+    }
+    bool IsEmpty() const {
+        if (elements_.size() == 0) return true;
+        return false;
+    }
+
+private:
+    vector<Type> elements_;
+};
+
+template <typename Type>
+class SortedStack {
+public:
+    void Push(const Type& element) {
+        if (elements_.IsEmpty()) {
+            elements_.Push(element);
+            return;
+        }
+
+        Type last_element = elements_.Peek();
+        if (last_element < element) {
+            elements_.Pop();
+            Push(element);
+            elements_.Push(last_element);
+        }
+        else {
+            elements_.Push(element);
+        }
+    }
+    void Pop() {
+        elements_.Pop();
+    }
+    const Type& Peek() const {
+        return elements_.Peek();
+    }
+    Type& Peek() {
+        return elements_.Peek();
+    }
+    void Print() const {
+        elements_.Print();
+    }
+    uint64_t Size() const {
+        return elements_.Size();
+    }
+    bool IsEmpty() const {
+        return elements_.IsEmpty();
+    }
+private:
+    Stack<Type> elements_;
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    SortedStack<int> stack;
+    vector<int> values(5);
+
+    // заполняем вектор для тестирования нашего стека
+    iota(values.begin(), values.end(), 1);
+    // перемешиваем значения
+    random_shuffle(values.begin(), values.end()); //random_shuffle C++14 !!!
+
+    // заполняем стек и проверяем, что сортировка сохраняется после каждой вставки
+    for (int i = 0; i < 5; ++i) {
+        cout << "Вставляемый элемент = "s << values[i] << endl;
+        stack.Push(values[i]);
+        stack.Print();
+    }
+}
