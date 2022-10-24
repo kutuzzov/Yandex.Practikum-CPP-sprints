6c939e60b8a6501aa4c94feae2038e09ab35535f
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2022-10-24 11:16:58 +0300

    Create Chapter 21 Lesson 2-1.cpp

---
+++ Sprint 4/Chapter 21 Lesson 2-1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename It>
+void PrintRange(It range_begin, It range_end) {
+    for (auto it = range_begin; it != range_end; ++it) {
+        cout << *it << " "s;
+    }
+    cout << endl;
+}
+
+template <typename Type>
+class Stack {
+public:
+    void Push(const Type& element) {
+        elements_.push_back(element);
+    }
+    void Pop() {
+        elements_.pop_back();
+    }
+    const Type& Peek() const {
+        return elements_.back();
+    }
+    Type& Peek() {
+        return elements_.back();
+    }
+    void Print() const {
+        PrintRange(elements_.begin(), elements_.end());
+    }
+    uint64_t Size() const {
+        return elements_.size();
+    }
+    bool IsEmpty() const {
+        return elements_.empty();
+    }
+
+private:
+    vector<Type> elements_;
+};
+
+int main() {
+    Stack<int> stack;
+    for (uint32_t i = 0; i < 10; ++i) {
+        stack.Push(i);
+        stack.Print();
+    }
+    while (!stack.IsEmpty()) {
+        stack.Pop();
+        stack.Print();
+    }
+}
