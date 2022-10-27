143e42c7e3cc1d69f657641710f1bf13a62b4ec7
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2022-10-27 11:23:24 +0300

    Create wall.h

---
+++ Sprint 4/Chapter 22 Lesson 4-2/wall.h
@@ -0,0 +1,22 @@
+class Wall {
+public:
+    enum class Color {
+        BLUE,
+        GREEN,
+        RED,
+        WHITE,
+        YELLOW
+    };
+
+    Wall(double width, double height);
+
+    double GetHeight() const;
+    double GetWidth() const;
+    void SetColor(Color color);
+		Color GetColor() const;
+
+private:
+    double width_;
+    double height_;
+    Color color_;
+};
