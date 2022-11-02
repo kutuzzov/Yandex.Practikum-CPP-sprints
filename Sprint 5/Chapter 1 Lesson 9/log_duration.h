7c90497d5ee3f6a941d97262fe1b3eb21015bdc8
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2022-11-02 16:01:22 +0300

    Create log_duration.h

---
+++ Sprint 5/Chapter 1 Lesson 9/log_duration.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+
+#define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
+#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+
+class LogDuration {
+public:
+    // заменим имя типа std::chrono::steady_clock
+    // с помощью using для удобства
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(const std::string& id) : id_(id) {
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        std::cerr << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    }
+
+private:
+    const std::string id_;
+    const Clock::time_point start_time_ = Clock::now();
+};
