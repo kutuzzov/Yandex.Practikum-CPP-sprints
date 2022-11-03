ee27ed88f66c9aba0bd253f5d3169417904a31ae
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2022-11-03 12:27:49 +0300

    Create Chapter 2 Lesson 5.cpp

---
+++ Sprint 5/Chapter 2 Lesson 5.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+// функция возвращает true, если векторы из одинаковых элементов
+// перепишите эту функцию, улучшив её асимптотическую сложность
+bool TestPermut(const vector<int>& v1, const vector<int>& v2) {
+    if (v1.size() != v2.size()) return false;
+
+    vector<int> sort_v1 = v1;
+    vector<int> sort_v2 = v2;
+
+    sort(sort_v1.begin(), sort_v1.end());
+    sort(sort_v2.begin(), sort_v2.end());
+
+    return sort_v1 == sort_v2;
+}
+
+int main() {
+    std::mt19937 g;
+
+    int n;
+    cin >> n;
+    vector<int> v1, v2;
+    v1.reserve(n);
+    v2.reserve(n);
+
+    for (int i = 0; i < n; ++i) {
+        v1.push_back(rand());
+        v2.push_back(rand());
+    }
+
+    // оба вектора случайны, вряд ли они совпадут
+    cout << "Random vectors match? "s << flush;
+    cout << (TestPermut(v1, v2) ? "Yes"s : "No"s) << endl;
+
+    // делаем один перестановкой другого явным образом
+    v2 = v1;
+    shuffle(v2.begin(), v2.end(), g);
+    cout << "Permuted vectors match? "s << flush;
+    cout << (TestPermut(v1, v2) ? "Yes"s : "No"s) << endl;
+}
