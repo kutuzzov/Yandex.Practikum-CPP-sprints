ed347b44d2c3549ec93b55621e7d8b974df1377e
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2022-12-12 12:32:27 +0300

    Create Chapter 6 Lesson 2.cpp

---
+++ Sprint 6/Chapter 6 Lesson 2.cpp
@@ -0,0 +1,58 @@
+#include <cassert>
+#include <cstddef>
+#include <string>
+#include <utility>
+
+template <typename Type>
+class SingleLinkedList {
+    // Узел списка
+    struct Node {
+        Node() = default;
+        Node(const Type& val, Node* next)
+            : value(val)
+            , next_node(next) {
+        }
+        Type value;
+        Node* next_node = nullptr;
+    };
+
+public:
+    SingleLinkedList() 
+        : size_(0)
+    {
+    }
+
+    // Возвращает количество элементов в списке за время O(1)
+    [[nodiscard]] size_t GetSize() const noexcept {
+        return size_;
+    }
+
+    // Сообщает, пустой ли список за время O(1)
+    [[nodiscard]] bool IsEmpty() const noexcept {
+        return size_ == 0;
+    }
+
+private:
+    // Фиктивный узел, используется для вставки "перед первым элементом"
+    Node head_;
+    size_t size_;
+};
+
+void Test0() {
+    using namespace std;
+    {
+        const SingleLinkedList<int> empty_int_list;
+        assert(empty_int_list.GetSize() == 0u);
+        assert(empty_int_list.IsEmpty());
+    }
+
+    {
+        const SingleLinkedList<string> empty_string_list;
+        assert(empty_string_list.GetSize() == 0u);
+        assert(empty_string_list.IsEmpty());
+    }
+}
+
+int main() {
+    Test0();
+}
