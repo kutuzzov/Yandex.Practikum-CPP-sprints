691032b230775afa1d9d68b3736b56ed0acb1b1f
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2023-01-08 00:15:54 +0300

    Add files via upload

---
+++ Sprint 7/Chapter 9 Lesson 10/simple_vector.h
@@ -0,0 +1,141 @@
+#pragma once
+
+#include "array_ptr.h"
+
+#include <cassert>
+#include <initializer_list>
+#include <stdexcept>
+
+template <typename Type>
+class SimpleVector {
+public:
+    using Iterator = Type*;
+    using ConstIterator = const Type*;
+
+    SimpleVector() noexcept = default;
+
+    // Создаёт вектор из size элементов, инициализированных значением по умолчанию
+    explicit SimpleVector(size_t size)
+        : SimpleVector(size, Type())
+    {
+    }
+
+    // Создаёт вектор из size элементов, инициализированных значением value
+    SimpleVector(size_t size, const Type& value)
+        : items_(size), size_(size), capacity_(size)
+    {
+        std::fill(items_.Get(), items_.Get() + size, value);
+    }
+
+    // Создаёт вектор из std::initializer_list
+    SimpleVector(std::initializer_list<Type> init)
+        : items_(init.size()), size_(init.size()), capacity_(init.size())
+    {
+        std::copy(init.begin(), init.end(), items_.Get());
+    }
+
+    // Возвращает количество элементов в массиве
+    size_t GetSize() const noexcept {
+        return size_;
+    }
+
+    // Возвращает вместимость массива
+    size_t GetCapacity() const noexcept {
+        return capacity_;
+    }
+
+    // Сообщает, пустой ли массив
+    bool IsEmpty() const noexcept {
+        return size_ == 0;
+    }
+
+    // Возвращает ссылку на элемент с индексом index
+    Type& operator[](size_t index) noexcept {
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    const Type& operator[](size_t index) const noexcept {
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    Type& At(size_t index) {
+        if(index >= size_) throw std::out_of_range("Out of range");
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    const Type& At(size_t index) const {
+        if(index >= size_) throw std::out_of_range("Out of range");
+        return items_[index];
+    }
+
+    // Обнуляет размер массива, не изменяя его вместимость
+    void Clear() noexcept {
+        size_ = 0;
+    }
+
+    // Изменяет размер массива.
+    // При увеличении размера новые элементы получают значение по умолчанию для типа Type
+    void Resize(size_t new_size) {
+        if (new_size <= size_) {
+            size_ = new_size;
+        }
+        if (new_size <= capacity_) {
+            std::fill(items_.Get() + size_, items_.Get() + size_ + new_size, Type());
+        }
+        if (new_size > capacity_) {
+            size_t new_capacity = std::max(new_size, capacity_ * 2);
+            ArrayPtr<Type> temp(new_capacity);
+            std::fill(temp.Get(), temp.Get() + new_capacity, Type());
+            std::copy(items_.Get(), items_.Get() + capacity_, temp.Get());
+            items_.swap(temp);
+            
+            size_ = new_size;
+            capacity_ = new_capacity;
+        }
+    }
+
+    // Возвращает итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator begin() noexcept {
+        return items_.Get();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator end() noexcept {
+        return items_.Get() + size_;
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator begin() const noexcept {
+        return items_.Get();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator end() const noexcept {
+        return items_.Get() + size_;
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cbegin() const noexcept {
+        return begin();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cend() const noexcept {
+        return end();
+    }
+private:
+    ArrayPtr<Type> items_;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+};
---
+++ Sprint 7/Chapter 9 Lesson 10/tests.h
@@ -0,0 +1,117 @@
+#pragma once
+#include <cassert>
+#include <stdexcept>
+
+// У функции, объявленной со спецификатором inline, может быть несколько
+// идентичных определений в разных единицах трансляции.
+// Обычно inline помечают функции, чьё тело находится в заголовочном файле,
+// чтобы при подключении этого файла из разных единиц трансляции не возникало ошибок компоновки
+inline void Test1() {
+    // Инициализация конструктором по умолчанию
+    {
+        SimpleVector<int> v;
+        assert(v.GetSize() == 0u);
+        assert(v.IsEmpty());
+        assert(v.GetCapacity() == 0u);
+    }
+
+    // Инициализация вектора указанного размера
+    {
+        SimpleVector<int> v(5);
+        assert(v.GetSize() == 5u);
+        assert(v.GetCapacity() == 5u);
+        assert(!v.IsEmpty());
+        for (size_t i = 0; i < v.GetSize(); ++i) {
+            assert(v[i] == 0);
+        }
+    }
+
+    // Инициализация вектора, заполненного заданным значением
+    {
+        SimpleVector<int> v(3, 42);
+        assert(v.GetSize() == 3);
+        assert(v.GetCapacity() == 3);
+        for (size_t i = 0; i < v.GetSize(); ++i) {
+            assert(v[i] == 42);
+        }
+    }
+
+    // Инициализация вектора при помощи initializer_list
+    {
+        SimpleVector<int> v{1, 2, 3};
+        assert(v.GetSize() == 3);
+        assert(v.GetCapacity() == 3);
+        assert(v[2] == 3);
+    }
+
+    // Доступ к элементам при помощи At
+    {
+        SimpleVector<int> v(3);
+        assert(&v.At(2) == &v[2]);
+        try {
+            v.At(3);
+            assert(false);  // Ожидается выбрасывание исключения
+        } catch (const std::out_of_range&) {
+        } catch (...) {
+            assert(false);  // Не ожидается исключение, отличное от out_of_range
+        }
+    }
+
+    // Очистка вектора
+    {
+        SimpleVector<int> v(10);
+        const size_t old_capacity = v.GetCapacity();
+        v.Clear();
+        assert(v.GetSize() == 0);
+        assert(v.GetCapacity() == old_capacity);
+    }
+
+    // Изменение размера
+    {
+        SimpleVector<int> v(3);
+        v[2] = 17;
+        v.Resize(7);
+        assert(v.GetSize() == 7);
+        assert(v.GetCapacity() >= v.GetSize());
+        assert(v[2] == 17);
+        assert(v[3] == 0);
+    }
+    {
+        SimpleVector<int> v(3);
+        v[0] = 42;
+        v[1] = 55;
+        const size_t old_capacity = v.GetCapacity();
+        v.Resize(2);
+        assert(v.GetSize() == 2);
+        assert(v.GetCapacity() == old_capacity);
+        assert(v[0] == 42);
+        assert(v[1] == 55);
+    }
+    {
+        const size_t old_size = 3;
+        SimpleVector<int> v(3);
+        v.Resize(old_size + 5);
+        v[3] = 42;
+        v.Resize(old_size);
+        v.Resize(old_size + 2);
+        assert(v[3] == 0);
+    }
+
+    // Итерирование по SimpleVector
+    {
+        // Пустой вектор
+        {
+            SimpleVector<int> v;
+            assert(v.begin() == nullptr);
+            assert(v.end() == nullptr);
+        }
+
+        // Непустой вектор
+        {
+            SimpleVector<int> v(10, 42);
+            assert(v.begin());
+            assert(*v.begin() == 42);
+            assert(v.end() == v.begin() + v.GetSize());
+        }
+    }
+}
