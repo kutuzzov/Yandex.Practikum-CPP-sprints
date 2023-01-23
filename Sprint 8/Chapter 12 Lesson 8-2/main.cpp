86a34d22b2ecdd4742ff5a1098ceced87ab0f832
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2023-01-23 13:12:08 +0300

    Create main.cpp

---
+++ Sprint 8/Chapter 12 Lesson 8-2/main.cpp
@@ -0,0 +1,32 @@
+#include "process_queries.h"
+#include "search_server.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    SearchServer search_server("and with"s);
+    int id = 0;
+    for (
+        const string& text : {
+            "funny pet and nasty rat"s,
+            "funny pet with curly hair"s,
+            "funny pet and not very nasty rat"s,
+            "pet with rat and rat and rat"s,
+            "nasty rat with curly hair"s,
+        }
+        ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, { 1, 2 });
+    }
+    const vector<string> queries = {
+        "nasty rat -not"s,
+        "not very funny nasty pet"s,
+        "curly hair"s
+    };
+    for (const Document& document : ProcessQueriesJoined(search_server, queries)) {
+        cout << "Document "s << document.id << " matched with relevance "s << document.relevance << endl;
+    }
+    return 0;
+}
