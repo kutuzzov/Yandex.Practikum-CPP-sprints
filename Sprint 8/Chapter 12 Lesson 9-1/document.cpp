1fc8ed4d2050869f5f0b5f1b0c0b4892985026a5
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2023-01-23 14:30:06 +0300

    Add files via upload

---
+++ Sprint 8/Chapter 12 Lesson 9-1/document.cpp
@@ -0,0 +1,11 @@
+#include "document.h"
+
+using namespace std::string_literals;
+
+std::ostream& operator<<(std::ostream& out, const Document& document) {
+    out << "{ "s
+        << "document_id = "s << document.id << ", "s
+        << "relevance = "s << document.relevance << ", "s
+        << "rating = "s << document.rating << " }"s;
+    return out;
+}
---
+++ Sprint 8/Chapter 12 Lesson 9-1/string_processing.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <set>
+
+std::vector<std::string> SplitIntoWords(const std::string& text);
+
+template <typename StringContainer>
+std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    std::set<std::string> non_empty_strings;
+    for (const std::string& str : strings) {
+        if (!str.empty()) {
+            non_empty_strings.insert(str);
+        }
+    }
+    return non_empty_strings;
+}
