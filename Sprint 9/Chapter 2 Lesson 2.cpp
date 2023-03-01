afcef71ba6a298ee9556d2edbfedda9b33b152e7
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2023-03-01 12:24:33 +0300

    Create Chapter 2 Lesson 2.cpp

---
+++ Sprint 9/Chapter 2 Lesson 2.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+// не меняйте файлы json.h и json.cpp
+#include "json.h"
+
+using namespace std;
+
+struct Spending {
+    string category;
+    int amount;
+};
+
+int CalculateTotalSpendings(const vector<Spending>& spendings) {
+    return accumulate(
+        spendings.begin(), spendings.end(), 0, [](int current, const Spending& spending) {
+            return current + spending.amount;
+        });
+}
+
+string FindMostExpensiveCategory(const vector<Spending>& spendings) {
+    assert(!spendings.empty());
+    auto compare_by_amount = [](const Spending& lhs, const Spending& rhs) {
+        return lhs.amount < rhs.amount;
+    };
+    return max_element(begin(spendings), end(spendings), compare_by_amount)->category;
+}
+
+vector<Spending> LoadFromJson(istream& input) {
+    vector<Spending> result;
+    Document documents = Load(input);
+    for (auto& document : documents.GetRoot().AsArray()) {
+        string category = document.AsMap().at("category").AsString();
+        int amount = document.AsMap().at("amount").AsInt();
+        result.push_back({ category, amount });
+    }
+
+    return result;
+}
+
+int main() {
+    // не меняйте main
+    const vector<Spending> spendings = LoadFromJson(cin);
+    cout << "Total "sv << CalculateTotalSpendings(spendings) << '\n';
+    cout << "Most expensive is "sv << FindMostExpensiveCategory(spendings) << '\n';
+}
