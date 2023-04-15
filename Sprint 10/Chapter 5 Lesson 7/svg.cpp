fa69181556230d0c07a7beb4b2de05f7bdfd9745
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2023-04-15 12:26:15 +0300

    Add files via upload

---
+++ Sprint 10/Chapter 5 Lesson 7/svg.cpp
@@ -0,0 +1,116 @@
+#include "svg.h"
+
+namespace svg {
+
+    using namespace std::literals;
+
+    void Object::Render(const RenderContext& context) const {
+        context.RenderIndent();
+
+        // Делегируем вывод тега своим подклассам
+        RenderObject(context);
+
+        context.out << std::endl;
+    }
+
+    // ---------- Circle ------------------
+
+    Circle& Circle::SetCenter(Point center) {
+        center_ = center;
+        return *this;
+    }
+
+    Circle& Circle::SetRadius(double radius) {
+        radius_ = radius;
+        return *this;
+    }
+
+    void Circle::RenderObject(const RenderContext& context) const {
+        auto& out = context.out;
+        out << "<circle cx=\""sv << center_.x << "\" cy=\""sv << center_.y << "\" "sv;
+        out << "r=\""sv << radius_ << "\" "sv;
+        out << "/>"sv;
+    }
+
+    // ---------- Polyline ----------------
+
+    Polyline& Polyline::AddPoint(Point point) {
+        points_.push_back(std::move(point));
+        return *this;
+    }
+
+    void Polyline::RenderObject(const RenderContext& context) const {
+        auto& out = context.out;
+        out << "<polyline points=\""sv;
+        bool is_first = true;
+        for (auto& point : points_) {
+            if (is_first) {
+                out << point.x << "," << point.y;
+                is_first = false;
+            }
+            else {
+                out << " "sv << point.x << "," << point.y;
+            }
+        }
+        out << "\" />"sv;
+    }
+
+    // ---------- Text --------------------
+
+    Text& Text::SetPosition(Point pos) {
+        pos_ = pos;
+        return *this;
+    }
+
+    Text& Text::SetOffset(Point offset) {
+        offset_ = offset;
+        return *this;
+    }
+
+    Text& Text::SetFontSize(uint32_t size) {
+        size_ = size;
+        return *this;
+    }
+
+    Text& Text::SetFontFamily(std::string font_family) {
+        font_family_ = std::move(font_family);
+        return *this;
+    }
+
+    Text& Text::SetFontWeight(std::string font_weight) {
+        font_weight_ = std::move(font_weight);
+        return *this;
+    }
+
+    Text& Text::SetData(std::string data) {
+        data_ = std::move(data);
+        return *this;
+    }
+
+    void Text::RenderObject(const RenderContext& context) const {
+        auto& out = context.out;
+        out << "<text x=\""sv << pos_.x << "\" y=\""sv << pos_.y << "\" "sv;
+        out << "dx=\""sv << offset_.x << "\" dy=\""sv << offset_.y << "\" "sv;
+        out << "font-size=\""sv << size_ << "\""sv;
+        if (!font_family_.empty()) out << " font-family=\""sv << font_family_ << "\" "sv;
+        if (!font_weight_.empty()) out << "font-weight=\""sv << font_weight_ << "\""sv;
+        out << ">"sv << data_ << "</text>"sv;
+    }
+
+    // ---------- Document ----------------
+
+    void Document::AddPtr(std::unique_ptr<Object>&& obj) {
+        objects_.emplace_back(std::move(obj));
+    }
+
+    void Document::Render(std::ostream& out) const {
+        RenderContext ctx(std::cout, 2, 2);
+        out << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"sv << std::endl;
+        out << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"sv << std::endl;
+        for (const auto& obj : objects_) {
+            obj->Render(ctx);
+        }
+        out << "</svg>"sv;
+    }
+
+}  // namespace svg
