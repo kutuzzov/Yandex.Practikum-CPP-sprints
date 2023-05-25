e7a2f4516e26f9ba8209ed15008cc65c3e4a6396
Vadim Kutuzov <93603697+kutuzzov@users.noreply.github.com>
2023-05-25 09:07:43 +0300

    Create Chapter 8 Lesson 4.cpp

---
+++ Sprint 11/Chapter 8 Lesson 4.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+using namespace std;
+
+// реализуйте эту функцию:
+size_t GetFileSize(string file) {
+    fstream fin(file, ios::in);
+    if (fin) fin.seekg(0, ios::end);
+    else return string::npos;
+    return fin.tellg();
+}
+
+int main() {
+    ofstream("test.txt") << "123456789"sv;
+    assert(GetFileSize("test.txt"s) == 9);
+
+    ofstream test2("test2.txt"s);
+    test2.seekp(1000);
+    test2 << "abc"sv;
+    test2.flush();
+
+    assert(GetFileSize("test2.txt"s) == 1003);
+    assert(GetFileSize("a file not exists"s) == string::npos);
+}
